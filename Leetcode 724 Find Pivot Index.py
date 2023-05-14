d6cdaa3e34c1d2f187abab0b4f4911bdc817e584
Reza <seda1390@gmail.com>
2023-05-14 13:10:33 -0700

    Update Leetcode 724 Find Pivot Index.py
    Adding new solution

--- Leetcode 724 Find Pivot Index.py
+++ Leetcode 724 Find Pivot Index.py
@@ -45,5 +45,20 @@ Constraints:
       prefix += num
 
     return -1
+
+'''
+# Second solution
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total= sum(nums)
+        temp = 0
+        for i in range(len(nums)):
+            if(nums[i] == total - 2*temp): 
+                return i
+            temp += nums[i]
+        return -1
+
+'''
  
  
