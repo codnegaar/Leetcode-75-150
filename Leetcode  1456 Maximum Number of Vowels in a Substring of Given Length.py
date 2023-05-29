01012ff3b6f7cf97c96d06d69c1ab346cbd600cd
Reza <seda1390@gmail.com>
2023-05-28 17:56:06 -0700

    Update Leetcode  1456 Maximum Number of Vowels in a Substring of Given Length.py

--- Leetcode  1456 Maximum Number of Vowels in a Substring of Given Length.py
+++ Leetcode  1456 Maximum Number of Vowels in a Substring of Given Length.py
@@ -23,4 +23,19 @@ Constraints:
         1 <= s.length <= 105
         s consists of lowercase English letters.
         1 <= k <= s.length
-''''
+'''
+class Solution:
+  def maxVowels(self, s: str, k: int) -> int:
+    ans = 0
+    maxi = 0
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    for i, c in enumerate(s):
+      if c in vowels:
+        maxi += 1
+      if i >= k and s[i - k] in vowels:
+        maxi -= 1
+      ans = max(ans, maxi)
+
+    return ans
+
