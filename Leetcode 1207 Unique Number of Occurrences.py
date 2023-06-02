a05bbcb187c997e2a05c3b4c713a8d2ce2705365
Reza <seda1390@gmail.com>
2023-06-02 10:12:40 -0700

    Create Leetcode 1207 Unique Number of Occurrences.py

---
+++ Leetcode 1207 Unique Number of Occurrences.py
@@ -0,0 +1,42 @@
+'''
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise. 
+
+Example 1:
+        Input: arr = [1,2,2,1,1,3]
+        Output: true
+        Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+        
+Example 2:
+        Input: arr = [1,2]
+        Output: false
+        
+Example 3:
+        Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+        Output: true
+
+Constraints:
+        1 <= arr.length <= 1000
+        -1000 <= arr[i] <= 1000
+
+'''
+
+class Solution:
+  def uniqueOccurrences(self, arr: List[int]) -> bool:
+
+    # class collections.Counter([iterable-or-mapping])
+    count = collections.Counter(arr)
+    occurrences = set()
+
+    for value in count.values():
+      if value in occurrences:
+        return False
+      occurrences.add(value)
+      
+    return True
+
+# Secon Solution 
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+      # class collections.Counter([iterable-or-mapping])
+      c = collections.Counter(arr)   
+      return len(set(c.values())) == len(c)
