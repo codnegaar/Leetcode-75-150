ca9279eeeb5f3c1b676d4edb0e3accb1aab34c21
Reza <seda1390@gmail.com>
2023-07-09 14:42:48 -0700

    Create Leetcode 435 Non-overlapping Intervals.py

---
+++ Leetcode 435 Non-overlapping Intervals.py
@@ -0,0 +1,42 @@
+'''
+435 Non-overlapping Intervals
+
+Given an array of intervals where intervals[i] = [starti, endi], return the minimum number of intervals you 
+need to remove to make the rest of the intervals non-overlapping.
+
+Example 1:
+        Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+        Output: 1
+        Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+Example 2:
+        Input: intervals = [[1,2],[1,2],[1,2]]
+        Output: 2
+        Explanation: You must remove two [1,2] to make the rest of the intervals non-overlapping.
+
+Example 3:
+        Input: intervals = [[1,2],[2,3]]
+        Output: 0
+        Explanation: You don't need to remove any intervals since they're already non-overlapping.
+         
+Constraints:
+        1 <= intervals.length <= 105
+        intervals[i].length == 2
+        -5 * 104 <= starti < endi <= 5 * 104
+
+'''
+
+class Solution:
+  def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+    ans = 0
+    currentEnd = -math.inf
+
+    for interval in sorted(intervals, key=lambda x: x[1]):
+      if interval[0] >= currentEnd:
+        currentEnd = interval[1]
+      else:
+        ans += 1
+
+    return ans
+
+
